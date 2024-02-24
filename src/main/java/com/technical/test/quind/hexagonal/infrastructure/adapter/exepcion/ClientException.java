98cd675fd38a212e6b88d9f1d6f41a98571ccc72
yeicamrojas <yarojasa2020-1@corhuila.edu.co>
2024-02-24 03:57:21 -0500

    Creación de Exepciones para respuestas que extienden de RuntimeException, con un controladorAdvice para el manejo de estos.

--- src/main/java/com/technical/test/quind/hexagonal/infrastructure/adapter/exepcion/ClientException.java
+++ src/main/java/com/technical/test/quind/hexagonal/infrastructure/adapter/exepcion/ClientException.java
@@ -1,4 +1,34 @@
 package com.technical.test.quind.hexagonal.infrastructure.adapter.exepcion;
 
-public class ClientException {
+import org.springframework.http.HttpStatus;
+
+public class ClientException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    private HttpStatus errorCode;
+    private String errorMessage;
+
+    public ClientException(HttpStatus errorCode, String errorMessage) {
+        this.errorCode = errorCode;
+        this.errorMessage = errorMessage;
+    }
+
+    public ClientException() {
+    }
+
+    public HttpStatus getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(HttpStatus errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
 }
--- src/main/java/com/technical/test/quind/hexagonal/infrastructure/rest/advice/MyControllerAdvice.java
+++ src/main/java/com/technical/test/quind/hexagonal/infrastructure/rest/advice/MyControllerAdvice.java
@@ -1,4 +1,24 @@
 package com.technical.test.quind.hexagonal.infrastructure.rest.advice;
 
+import com.technical.test.quind.hexagonal.infrastructure.adapter.exepcion.AccountException;
+import com.technical.test.quind.hexagonal.infrastructure.adapter.exepcion.ClientException;
+import com.technical.test.quind.hexagonal.infrastructure.adapter.exepcion.ProductException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class MyControllerAdvice {
+    @ExceptionHandler(AccountException.class)
+    public ResponseEntity<String> handleEmptyInput(AccountException emptyInputException){
+        return new ResponseEntity<String>(emptyInputException.getErrorMessage(), emptyInputException.getErrorCode());
+    }
+    @ExceptionHandler(ClientException.class)
+    public ResponseEntity<String> handleEmptyInput(ClientException emptyInputException){
+        return new ResponseEntity<String>(emptyInputException.getErrorMessage(), emptyInputException.getErrorCode());
+    }
+    @ExceptionHandler(ProductException.class)
+    public ResponseEntity<String> handleEmptyInput(ProductException emptyInputException){
+        return new ResponseEntity<String>(emptyInputException.getErrorMessage(), emptyInputException.getErrorCode());
+    }
 }
