d7ef9cd2f38cdb5b393f36d611a5d6efd440b298
yeicamrojas <yarojasa2020-1@corhuila.edu.co>
2024-02-24 04:46:19 -0500

    Creacion de Mapper por medio de infrastructure

--- src/main/java/com/technical/test/quind/hexagonal/infrastructure/adapter/mapper/AccountDboMapper.java
+++ src/main/java/com/technical/test/quind/hexagonal/infrastructure/adapter/mapper/AccountDboMapper.java
@@ -1,4 +1,20 @@
 package com.technical.test.quind.hexagonal.infrastructure.adapter.mapper;
 
+
+import com.technical.test.quind.hexagonal.domain.model.Account;
+import com.technical.test.quind.hexagonal.infrastructure.adapter.entity.AccountEntity;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
 public interface AccountDboMapper {
+
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "accountType",target = "accountType")
+    @Mapping(source = "product_id",target = "product_id")
+    AccountEntity toDbo(Account domain);
+
+    @InheritInverseConfiguration
+    Account toDomain(AccountEntity accountEntity);
 }
--- src/main/java/com/technical/test/quind/hexagonal/infrastructure/adapter/mapper/ClientDboMapper.java
+++ src/main/java/com/technical/test/quind/hexagonal/infrastructure/adapter/mapper/ClientDboMapper.java
@@ -1,4 +1,23 @@
 package com.technical.test.quind.hexagonal.infrastructure.adapter.mapper;
 
+import com.technical.test.quind.hexagonal.domain.model.Client;
+import com.technical.test.quind.hexagonal.infrastructure.adapter.entity.ClientEntity;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
 public interface ClientDboMapper {
+
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "identificationTypeEnum", target = "identificationTypeEnum")
+    @Mapping(source = "clientName", target = "clientName")
+    @Mapping(source = "clientSurname", target = "clientSurname")
+    @Mapping(source = "client_email", target = "clientEmail")
+    @Mapping(source = "date_of_birth", target = "dateOfBirth")
+    @Mapping(source = "productEntities", target = "productEntities")
+    ClientEntity toDbo (Client domain);
+
+    @InheritInverseConfiguration
+    Client toDomain(ClientEntity entity);
 }
