d152a6fca34395a3d87473de8d4a9d58340f2e16
yeicamrojas <yarojasa2020-1@corhuila.edu.co>
2024-02-25 00:58:01 -0500

    prueba unitaria para update, se realizo prueba en postman del servicio.

--- src/test/java/com/technical/test/quind/hexagonal/service/ClientManagementServiceTest.java
+++ src/test/java/com/technical/test/quind/hexagonal/service/ClientManagementServiceTest.java
@@ -9,7 +9,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+
+import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
@@ -33,13 +36,31 @@ public class ClientManagementServiceTest {
     }
 
     @Test
-    void createCliente() {
+    void createClient() {
         ClientDto clientDto = ClientDto.builder()
-                .dateOfBirth("2000-11-11")
+                .dateOfBirth("2000-01-01")
                 .build();
 
         ClientEntity clienteEntity = new ClientEntity();
         when(clienteRepository.save(clienteEntity)).thenReturn(clienteEntity);
         clientService.createClient(clientDto);
+    }
+    @Test
+    void createClientMinor() {
+        ClientDto clientDto = ClientDto.builder()
+                .dateOfBirth("2020-01-01")
+                .build();
+        clientService.createClient(clientDto);
+    }
+    @Test
+    void updateClient(){
+        ClientDto clientDto = ClientDto.builder()
+                .identificationNumber("1000381834")
+                .dateOfBirth("2003-09-03")
+                .build();
+        ClientEntity clientEntity = new ClientEntity();
+        Mockito.when(clienteRepository.findClientEntityByIdentificationNumber(clientDto.getIdentificationNumber())).thenReturn(Optional.of(clientEntity));
+        Mockito.when(clienteRepository.save(any(ClientEntity.class))).thenReturn(clientEntity);
+        clientService.updateClient(clientDto.getIdentificationNumber(), clientDto);
     }
 }
