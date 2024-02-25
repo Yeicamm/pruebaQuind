ee1ffeaf333b82a4b75bb7836c857a9af95e07ef
yeicamrojas <yarojasa2020-1@corhuila.edu.co>
2024-02-25 17:20:12 -0500

    Avances en test unitarios de Producto.

--- src/main/java/com/technical/test/quind/hexagonal/domain/model/dto/EditAccountStatusDto.java
+++ src/main/java/com/technical/test/quind/hexagonal/domain/model/dto/EditAccountStatusDto.java
@@ -3,11 +3,13 @@ package com.technical.test.quind.hexagonal.domain.model.dto;
 import com.technical.test.quind.hexagonal.domain.model.enums.AccountState;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class EditAccountStatusDto {
     private String accountNumber;
     @Enumerated(EnumType.STRING)
---
+++ src/test/java/com/technical/test/quind/hexagonal/service/ProductManagementServiceTest.java
@@ -0,0 +1,83 @@
+package com.technical.test.quind.hexagonal.service;
+
+import com.technical.test.quind.hexagonal.application.mapper.ProductMapper;
+import com.technical.test.quind.hexagonal.application.service.AccountManagementService;
+import com.technical.test.quind.hexagonal.application.service.ProductManagementService;
+import com.technical.test.quind.hexagonal.domain.model.dto.EditAccountStatusDto;
+import com.technical.test.quind.hexagonal.domain.model.dto.ProductDto;
+import com.technical.test.quind.hexagonal.domain.model.dto.request.RequestAccountClientDto;
+import com.technical.test.quind.hexagonal.infrastructure.adapter.entity.ProductEntity;
+import com.technical.test.quind.hexagonal.infrastructure.adapter.repository.AccountRepository;
+import com.technical.test.quind.hexagonal.infrastructure.adapter.repository.ProductRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+public class ProductManagementServiceTest {
+    @Mock
+    ProductRepository productRepository;
+    @Mock
+    private AccountManagementService accountManagementService;
+    @InjectMocks
+    private ProductManagementService productManagementService;
+
+
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void cannotCreateAccountSavings(){
+        ProductDto productDto = new ProductDto();
+        RequestAccountClientDto requestAccountClientDto = RequestAccountClientDto.builder()
+                .productDto(productDto)
+                .accountType("")
+                .build();
+        productManagementService.accountCreate(requestAccountClientDto);
+
+    }
+    @Test
+    void createAccountSavings(){
+        ProductDto productDto = new ProductDto();
+        RequestAccountClientDto requestAccountClientDto = RequestAccountClientDto.builder()
+                .productDto(productDto)
+                .accountType("SAVINGS")
+                .build();
+        productManagementService.accountCreate(requestAccountClientDto);
+    }
+    @Test
+    void createAccountCurrent(){
+        ProductDto productDto = new ProductDto();
+        RequestAccountClientDto requestAccountClientDto = RequestAccountClientDto.builder()
+                .productDto(productDto)
+                .accountType("CURRENT")
+                .build();
+        productManagementService.accountCreate(requestAccountClientDto);
+    }
+    @Test
+    void accountStateUpdateNotFound(){
+        EditAccountStatusDto editAccountStatusDto = EditAccountStatusDto.builder()
+                .accountNumber("53")
+                .build();
+        Mockito.when(productRepository.findProductEntityByAccountNumber(editAccountStatusDto.getAccountNumber())).thenReturn(Optional.empty());
+        productManagementService.accountStateUpdate(editAccountStatusDto);
+    }
+    @Test
+    void accountStateUpdateIsPresent(){
+        EditAccountStatusDto editAccountStatusDto = EditAccountStatusDto.builder()
+                .accountNumber("5312345678")
+                .build();
+        ProductEntity product = new ProductEntity();
+        product.setAccountNumber("5312345642");
+        Mockito.when(productRepository.findProductEntityByAccountNumber(editAccountStatusDto.getAccountNumber())).thenReturn(Optional.of(product));
+        productManagementService.accountStateUpdate(editAccountStatusDto);
+    }
+}
